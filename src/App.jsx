7556a5cfa06b862bc0604726fce99d235ea760a2
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-27 10:52:08 +0100

    implemented functionality to add task

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectSideBar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectState, setProjectState] = useState(
@@ -11,6 +12,22 @@ function App() {
       tasks: []
     }
   );
+
+  function handleAddTask(text) {
+    setProjectState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId
+      };
+
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks]
+      }
+    })
+  }
 
   function handleSelectProject(id) {
     setProjectState((prevState) => {
@@ -30,6 +47,16 @@ function App() {
     });
   }
 
+  const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId)
+
+  let content = (
+    <SelectedProject 
+      project={selectedProject} 
+      onAddTask={handleAddTask}
+      tasks={projectState.tasks}
+    />
+  );
+
 
   return (
     <main className="h-screen my-8 flex gap-8">
@@ -37,8 +64,10 @@ function App() {
       <ProjectSideBar
         onStartAddProject={handleStartAddProject}
         projects={projectState.projects}
-        onSelectProject={handleSelectProject} 
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectState.selectedProjectId} 
       />
+      
     </main>
   )
 }
